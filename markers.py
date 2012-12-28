04999016cd11a0b418d502eefc8476e0cc8f9890
Ankush Gupta <ankush.me@hotmail.com>
2012-12-28 18:11:30 +0530

    markers.py corrected. works.

--- markers.py
+++ markers.py
@@ -3,27 +3,59 @@ Simple script to display markers in rviz.
 Uses John's python code.
 """
 
-import brett2.ros_utils as ru
 import rospy
 import geometry_msgs.msg as gm
+import std_msgs.msg as stdm
+import numpy as np
+from visualization_msgs.msg import Marker
 
 
-rospy.init_node("markers1")
+class MarkerPlacer:
+    """
+    A very simple class to place markers in rviz.
+    Useful for visualizing transforms.
+    """
 
+    def __init__(self):
+        self.pub = rospy.Publisher('visualization_marker', Marker)
+        self.ids = set([])
 
-rviz = ru.RvizWrapper()
+    def draw_marker(self, ps, id=None, type=Marker.CUBE, ns='default_ns',rgba=(0,1,1,0.5), scale=(.1,.03,.03),text='',duration=0):
+        if id is None: id = self.get_unused_id()
+        marker        = Marker(type=type, action=Marker.ADD)
+        marker.ns     = ns
+        marker.header = ps.header
+        marker.pose   = ps.pose
+        marker.scale  = gm.Vector3(*scale)
+        marker.color  = stdm.ColorRGBA(*rgba)
+        marker.id     = id
+        marker.text   = text
+        marker.lifetime = rospy.Duration(duration)
+        self.pub.publish(marker)      
 
-ps = gm.PoseStamped()
-ps.header.frame_id = '/map'
-ps.header.stamp = rospy.Time.now()
+    def get_unused_id(self):
+        while True:
+            id = np.random.randint(0,2147483647)
+            if id not in self.ids: return id
+    
 
-# change ps.pose as desired
-ps.pose.position.x = 0
-ps.pose.position.y = 0
-ps.pose.position.z = 0
-ps.pose.orientation.x = 0.0
-ps.pose.orientation.y = 0.0
-ps.pose.orientation.z = 0.0
-ps.pose.orientation.w = 1.0
+rospy.init_node("markers")
+r = rospy.Rate(1)
+p = MarkerPlacer()
 
-rviz.draw_marker(ps, ns='markers1',id=0,scale=(1,1,1), duration=0)
+while True:
+    ps = gm.PoseStamped()
+    ps.header.frame_id = '/frame'
+    ps.header.stamp = rospy.Time.now()
+
+    # change ps.pose as desired
+    ps.pose.position.x = 0
+    ps.pose.position.y = 0
+    ps.pose.position.z = 0
+    ps.pose.orientation.x = 0.0
+    ps.pose.orientation.y = 0.0
+    ps.pose.orientation.z = 0.0
+    ps.pose.orientation.w = 1.0
+
+    p.draw_marker(ps,scale=(1,1,1), duration=0)
+    r.sleep()
