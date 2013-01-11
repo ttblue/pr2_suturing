b056f99be5523cfdc18b6f801c4339ba9ddedd1e
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-01-11 02:08:35 -0800

    Changed code such that once you pick up flap with some index, you can only continue the rest of the procedure with the corresponding index (you cannot suddenly switch sides). Testing transforms is not a class method anymore. Wrote tentative code for reorienting needle on piercing. Or at least, finding transforms for this.

--- test.py
+++ test.py
@@ -27,6 +27,9 @@ class SutureActionsPR2 (PlannerPR2):
         self.tf_listener.waitForTransform("/base_footprint", self.camera_frame, rospy.Time(), rospy.Duration(10.0))
         (trans, rot) = self.tf_listener.lookupTransform('/base_footprint',self.camera_frame,rospy.Time(0))
         self.camera_transform = conv.trans_rot_to_hmat(trans, rot);
+
+        # Initial index of cut picked up. Rest of procedure would be done taking this into account.
+        self.init_index = 0
         
         # Variables for flap pick-up
         # How far above the cut to start
@@ -50,7 +53,9 @@ class SutureActionsPR2 (PlannerPR2):
         # Distance to move towards hole from start position to pierce
         self.hole_moveToward = 0.025
         # Radius to move in circle to pierce cut
-        self.hole_finAng = np.pi/3 
+        self.hole_finAng = np.pi/3
+        # The initial hole pierce point. This is assumed to be a point on the needle during the pierce.
+        self.init_holePt = np.array([0,0,0])
         
     # Point would need to be midpoint    
     def getCutLine(self, index):
@@ -121,7 +126,7 @@ class SutureActionsPR2 (PlannerPR2):
         tfm[0:3,2] = np.unwrap(dVecZ)
         tfm[0:3,3] = self.camera_transform.dot(pt+[1])[:-1]
         
-        # self.testTransforms([tfm], ['cut_midpoint_tfm'], ['camera_rgb_optical_frame'])
+        # testTransforms([tfm], ['cut_midpoint_tfm'], ['camera_rgb_optical_frame'])
     
         print "Midpoint: ", self.camera_transform.dot(pt+[1])[:-1]
         
@@ -129,28 +134,31 @@ class SutureActionsPR2 (PlannerPR2):
         
     # TODO: Move gripper to some point along cut rather than midPoint
     # TODO: Rotation correction done only for left cut. Do for right cut (left gripper) 
-    def moveGripperToPickupStartPos (self, index, dist):
+    def moveGripperToPickupStartPos (self, dist=0.05):
         """
-        Moves appropriate gripper to cut specified by index.
+        Moves appropriate gripper to cut specified by init_index.
         dist is the distance from the midpoint along the cut where
         the PR2 should pick up the flap. +ve direction is away from the PR2.
         """
-        cutTfm = self.findCutTransform(index)
+        if self.init_index == 0:
+            rospy.logwarn("Procedure has not been initialized properly. Please call the function pickUpFlap or runThrough instead.")
+            return
+        cutTfm = self.findCutTransform(self.init_index)
         if cutTfm is None:
             rospy.logerr("Cannot find cut Transform for the index")
             return False
         
         self.update_rave()
-        arm = {1:self.rarm, 2:self.larm}[index]
+        arm = {1:self.rarm, 2:self.larm}[self.init_index]
         
         # Close gripper before starting
-        gripper = {1:self.rgrip, 2:self.lgrip}[index]
+        gripper = {1:self.rgrip, 2:self.lgrip}[self.init_index]
         gripper.close()
         self.join_all()
         rospy.sleep(1)
 
         # Flip orientation of several things according to cut        
-        flip = {1:1, 2:-1}[index]
+        flip = {1:1, 2:-1}[self.init_index]
         
         dVecX = cutTfm[0:3,0]
         dVecY = cutTfm[0:3,1]
@@ -171,25 +179,28 @@ class SutureActionsPR2 (PlannerPR2):
         gpTfm = cutTfm.dot(corrRot.dot(corrRot2))
         
         # For testing the transforms:
-        # self.testTransforms ([gpTfm], ['gripper_tfm'], ['base_footprint'])
+        # testTransforms ([gpTfm], ['gripper_tfm'], ['base_footprint'])
         
         arm.goto_pose_matrix(gpTfm, 'base_footprint', 'end_effector')
         self.join_all()
         return True
         
-    def moveGripperToPickupEndPos (self, index):
+    def moveGripperToPickupEndPos (self):
         """
         Assumes that the gripper is now holding the flap in the same
         orientation as the start position.
         ONLY call this if the above assumption is true.
         """
+        if self.init_index == 0:
+            rospy.logwarn("Procedure has not been initialized properly. Please call the function pickUpFlap or runThrough instead.")
+            return
         self.update_rave()
-        arm = {1:self.rarm, 2:self.larm}[index]
+        arm = {1:self.rarm, 2:self.larm}[self.init_index]
         # To move into the cut, in case it is useful
-        # moveInDir = {1:'l', 2:'r'}[index]
+        # moveInDir = {1:'l', 2:'r'}[self.init_index]
     
         # Removed the following to make more space for the needle
-        # flip = {1:1, 2:-1}[index]
+        # flip = {1:1, 2:-1}[self.init_index]
         # gpTfm = arm.manip.GetEndEffectorTransform() 
         # Assuming gripper is at an angle, twist it all the way to pi (or -pi)
         # corrRot = np.array ([[1, 0                                            , 0                                             , 0],
@@ -199,7 +210,7 @@ class SutureActionsPR2 (PlannerPR2):
         # gpTfm = gpTfm.dot(corrRot)
         
         # For testing the transforms:
-        # self.testTransforms ([gpTfm], ['gripper_end_tfm'], ['base_footprint'])
+        # testTransforms ([gpTfm], ['gripper_end_tfm'], ['base_footprint'])
         
         # arm.goto_pose_matrix(gpTfm, 'base_footprint', 'end_effector')
         # self.join_all()
@@ -221,6 +232,10 @@ class SutureActionsPR2 (PlannerPR2):
         Function to make PR2 pick up flap of cut represented by index.
         dist -> distance along cut (away from PR2) to pick up cut.
         """
+        if index not in [1, 2]:
+            rospy.logwarn("This has only been implemented for one cut with two sides. The index must be 1 or 2.")
+            return
+        self.init_index = index
         gripper = {1:self.rgrip, 2:self.lgrip}[index]
         arm = {1:self.rarm, 2:self.larm}[index]
         moveInDir = {1:'l', 2:'r'}[index]
@@ -230,12 +245,14 @@ class SutureActionsPR2 (PlannerPR2):
         rospy.loginfo("Going to the start position for picking up the flap.")
         
         try:
-            ready = self.moveGripperToPickupStartPos(index, dist)
+            ready = self.moveGripperToPickupStartPos(dist)
             if not ready:
                 rospy.logerr("Unable to move to start position to pick up flap.")
+                self.init_index = 0
                 return
         except IKFail:
             rospy.logerr("Unable to move to start position to pick up flap. IK Failed.")
+            self.init_index = 0
             return
             
         rospy.sleep(7)
@@ -257,6 +274,7 @@ class SutureActionsPR2 (PlannerPR2):
             except IKFail:
                 if count==attempts:
                     rospy.logerr("Unable to move down. IK Failed.")
+                    self.init_index = 0
                     return
                 else:
                     pass
@@ -272,12 +290,14 @@ class SutureActionsPR2 (PlannerPR2):
             rospy.sleep(4)
         except IKFail:
             rospy.logerr("Unable to pick up flap. IK Failed.")
+            self.init_index = 0
             return
         #When tested:
         try:
             self.moveGripperToPickupEndPos(index)
         except IKFail:
             rospy.logerr("Unable to move to final position. IK Failed.")
+            self.init_index = 0
         
     def findHoleTransform (self, index):
         """
@@ -311,32 +331,35 @@ class SutureActionsPR2 (PlannerPR2):
         WfmNTip[0:3,3] = self.camera_transform.dot(pt+[1])[:-1]
 
         # For testing the transforms:
-        # self.testTransforms ([WfmNTip], ['needle_tip_tfm'], ['base_footprint'])
+        # testTransforms ([WfmNTip], ['needle_tip_tfm'], ['base_footprint'])
 
         return WfmNTip
     
     # TODO: Maybe start a little way away from the hole to begin with
     # Might not use this if planning entire path is needed beforehand
-    def pierceHole(self, index):
+    def pierceHole(self):
         """
         # Moves the gripper to the starting position to pierce the 
-        # hole specified by the index.
+        # hole specified by  self.init_index.
         # This is assuming the needle is already being held by the
         # gripper.
         
-        Function to make the PR2 pierce the hole specified by index.
+        Function to make the PR2 pierce the hole specified by self.init_index.
         If pi/4 works, stick with it (maybe not)
         """
+        if self.init_index == 0:
+            rospy.logwarn("Flap not picked up. Or self.init_index incorrectly set. Please call the function pickUpFlap or runThrough instead.")
+            return
         self.enableSponge(True) 
         self.update_rave()
         
-        holeTfm = self.findHoleTransform(index)
+        holeTfm = self.findHoleTransform(self.init_index)
         if holeTfm is None:
             rospy.logerr("Cannot find hole Transform for the index")
             return False
         
-        arm = {1:self.larm, 2:self.rarm}[index]
-        # flip = {1:1, 2:-1}[index]
+        arm = {1:self.larm, 2:self.rarm}[self.init_index]
+        # flip = {1:1, 2:-1}[self.init_index]
         
         # Openrave needle. Assuming for now that we're working only with left hand
         self.grabNeedle()#('l',2)
@@ -355,8 +378,12 @@ class SutureActionsPR2 (PlannerPR2):
         
         gpTfm[0:3,3] += self.hole_moveToward*holeTfm.dot(rotX)[0:3,2]
         
+        # Store initial pierce point
+        self.update_rave()
+        self.init_holePt = self.needleTipTransform()[0:3,3]
+        
         # For testing the transforms:
-        # self.testTransforms ([gpTfm], ['new_needle'], ['base_footprint'])
+        # testTransforms ([gpTfm], ['new_needle'], ['base_footprint'])
         
         arm.goto_pose_matrix (gpTfm, 'base_footprint', 'end_effector')
         self.join_all()
@@ -370,18 +397,21 @@ class SutureActionsPR2 (PlannerPR2):
     
     # Decided to do everything in this function in order to be able to plan entire
     # path and see if there is an IKFail
-    def pierceHole2 (self, index):
+    def pierceHole2 (self):
         """
-        Function to make the PR2 pierce the hole specified by index.
+        Function to make the PR2 pierce the hole specified by self.init_index.
         This function checks a bunch of viable positions and chooses the
         one which has the needle closest to the normal.
         """
+        if self.init_index == 0:
+            rospy.logwarn("Flap not picked up. Or self.init_index incorrectly set. Please call the function pickUpFlap or runThrough instead.")
+            return
         self.enableSponge(True)
-        arm = {1:self.larm, 2:self.rarm}[index]
-        # flip = {1:1, 2:-1}[index]
+        arm = {1:self.larm, 2:self.rarm}[self.init_index]
+        # flip = {1:1, 2:-1}[self.init_index]
         self.update_rave()
         
-        holeTfm = self.findHoleTransform(index)
+        holeTfm = self.findHoleTransform(self.init_index)
         if holeTfm is None:
             rospy.logerr("Cannot find hole Transform for the index")
             return
@@ -401,7 +431,7 @@ class SutureActionsPR2 (PlannerPR2):
         attempts = 15 
         dAng = np.pi/(4*attempts)
         # If using right arm
-        if index == 2:
+        if self.init_index == 2:
             pose = {1:2, 2:1, 3:4, 4:3}[self.sneedle_pose]
         else:
             pose = self.sneedle_pose
@@ -435,11 +465,14 @@ class SutureActionsPR2 (PlannerPR2):
                 else:
                     pass
         
-        # self.testTransforms([gpTfm], ['pierce_frame'], ['base_footprint'])
+        # testTransforms([gpTfm], ['pierce_frame'], ['base_footprint'])
         
         # Now that PR2 is ready, ask it to pierce the flap 
         # (and the planner has returned something feasible)
+        
+        # Store initial pierce point
         self.update_rave()
+        self.init_holePt = self.needleTipTransform()[0:3,3]
         # Last saved value of gpTfm
         arm.goto_pose_matrix (gpTfm, 'base_footprint', 'end_effector')
         self.join_all()
@@ -449,13 +482,103 @@ class SutureActionsPR2 (PlannerPR2):
         self.join_all()
         rospy.sleep(7)
         
+    # Probably the most math-heavy bit.
+    # Might be a bit too elaborate.
+    # Check and re-check this. It's definitely going to be wrong.
+    def reorientAfterPiercing (self):
+        """
+        Rotates the needle about the entry point in order to be able to exit
+        from the exit point.
+        First, it orients the needle such that the exit point is in the plane
+        the needle is in (rotation about base_footprint's z axis).
+        Then it orients itself about the needle x-axis such that the needle
+        can pierce the exit point if it continues in a circle.
+        """
+        self.update_rave()
+        newIndex      =  {1:2, 2:1}[self.init_index]
+        holeTfm2      =  self.getHoleNormal(newIndex)
+        holePt        =  holeTfm2[0:3,3]
+        currNeedleTfm =  self.needleTipTransform()
+        # Step 1: Rotation about z-axis
+        
+        # i)    Calculate angle to rotate about
+        #       Do this using the formula you derived. Take care of the signs of theta and phi.
+        # Vector from first hole to second
+        v = holePt - self.init_holePt
+        # Current x basis vector in needle frame (perpendicular to needle plane)
+        x = currNeedleTfm[0:3,0]
+        # Values to make calculation easier to find angle to rotate
+        a1 = v[0]*x[0] + v[1]*x[1]
+        a2 = v[1]*x[0] - v[0]*x[1] 
+        b  = v[2]*x[2]
+        
+        # Angle to rotate is the one which makes x perpendicular to v
+        a  = np.sqrt(a1**2 + a2**2)
+        phi = np.arcsin(a1/a)
+        if np.sign(np.cos(phi)) != np.sign(a2):
+            phi = np.pi - phi
+        thetaZ = np.arcsin(-b/a) - phi
+        
+        # Move to closest solution, assuming the needle is already close to correct 
+        # orientation
+        while thetaZ >= np.pi/2:
+            thetaZ -= np.pi
+        while thetaZ <= -np.pi/2:
+            thetaZ += np.pi
+        
+        # ii)   Translate to Origin
+        toOriginTfm = np.eye(4)
+        toOriginTfm[0:3,3] -= np.unwrap(holePt)
+        # iii)  Rotate about z
+        rotZ = np.eye(4)
+        ind1, ind2 = [0,0,1,1], [0,1,0,1]
+        rotZ[ind1,ind2] = np.array([np.cos(thetaZ), -1*np.sin(thetaZ), np.sin(thetaZ), np.cos(thetaZ)])
+        # iv)   Translate back
+        fromOriginTfm = np.eye(4)
+        fromOriginTfm[0:3,3] += np.unwrap(holePt)
+        
+        # First rotation about z
+        firstRotTfm = fromOriginTfm.dot(rotZ.dot(toOriginTfm))
+         
+        # Step 2: Rotation about x-axis
+        
+        # i)    Find radius to be a distance of self.sneedle_radius along the y-direction of 
+        #       the needle tip frame, from the needle.
+        midNeedleTfm = firstRotTfm.dot(currNeedleTfm)
+        x_m = midNeedleTfm[0:3,0]
+        y_m = midNeedleTfm[0:3,1]
+        initCenter   = midNeedleTfm[0:3,3] + self.sneedle_radius*y_m
+        n = np.cross(v, x_m)
+        
+        # ii)   Let initCenterVec be the vector from the entry point to the center. Calculate 
+        #       the current angle of this vector from some fixed vector in the needle plane. 
+        #       This needs to become a new angle such that the tip can pierce the other hole.
+        initCenterVec = initCenter - self.init_holePt
+        initAng = np.arccos (initCenterVec.dot(v)/(nla.norm(initCenterVec, 2)*nla.norm(v,2)))
+        initAng *= np.sign(n.dot(initCenterVec))
+        finAng = np.arccos (nla.norm(v,2)/(2*self.sneedle_radius))
+        
+        # iii)  Rotate about the x of this axis.
+        # Want to rotate in the negative direction of this angle because counter-clockwise is +ve
+        thetaX = -(finAng - initAng)
+        rotX = np.eye(4)
+        ind1, ind2 = [1,1,2,2], [1,2,1,2]
+        rotX[ind1,ind2] = np.array([np.cos(thetaX), -1*np.sin(thetaX), np.sin(thetaX), np.cos(thetaX)])
+        
+        finalNeedleTfm = firstRotTfm.dot(currNeedleTfm.dot(rotX))
+              
+        testTransforms([currNeedleTfm, finalNeedleTfm], ['init_needletfm', 'final_needletfm'],['base_footprint', 'base_footprint'])
+        
     def runThrough (self, index, dist=0.05):
         """
         Picks up indexed flap and pierces indexed cut.
         Distance refers to the distance from the hole to pick up the flap.
         """
         self.pickUpFlap(index, dist)
-        self.pierceHole2(index)
+        if self.init_index == 0:
+            return
+        self.pierceHole2()
+        self.reorientAfterPiercing()
         
         
     def testNeedleTfm (self):
@@ -464,31 +587,31 @@ class SutureActionsPR2 (PlannerPR2):
         """
         self.update_rave()
         sTfm = self.needleTipTransform()
-        self.testTransforms ([sTfm], ['needle_tip'], ['base_footprint'])
+        testTransforms ([sTfm], ['needle_tip'], ['base_footprint'])
         
-    def testTransforms (self, tfms, child_frames, parent_frames, time = 25):
-        """
-        Basic code to test transforms. Visualize on RViz or something.
-        """
-        br = tf.TransformBroadcaster()
-        rate = rospy.Rate(0.2)
-        
-        initTime  = rospy.Time.now()
-        totTime = rospy.Duration(time)
-        
-        if not time:
-            foreverCheck = True
-        else:
-            foreverCheck = False
-        
-        while foreverCheck or (rospy.Time.now() - initTime < totTime):
-            for tfm, child_frame, parent_frame in zip(tfms, child_frames,parent_frames):
-                (trans, rot) = conv.hmat_to_trans_rot(tfm)
-                br.sendTransform(trans, rot,
-                                 rospy.Time.now(),
-                                 child_frame,
-                                 parent_frame)
-                rate.sleep()
+def testTransforms (tfms, child_frames, parent_frames, time = 25):
+    """
+    Basic code to test transforms. Visualize on RViz or something.
+    """
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(0.2)
+    
+    initTime  = rospy.Time.now()
+    totTime = rospy.Duration(time)
+
+    if not time:
+        foreverCheck = True
+    else:
+        foreverCheck = False
+    
+    while foreverCheck or (rospy.Time.now() - initTime < totTime):
+        for tfm, child_frame, parent_frame in zip(tfms, child_frames,parent_frames):
+            (trans, rot) = conv.hmat_to_trans_rot(tfm)
+            br.sendTransform(trans, rot,
+                             rospy.Time.now(),
+                             child_frame,
+                             parent_frame)
+            rate.sleep()
         
 
 def waitAndClose(t):
